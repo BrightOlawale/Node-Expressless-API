9370ec9aca58051f31dac66901236f0a109f9afa
BrightOlawale <oolawalebright@gmail.com>
2023-05-01 23:28:19 +0100

    feat: route to token created

--- index.js
+++ index.js
@@ -104,5 +104,6 @@ const commonServer = function(req, res){
 // Handle routing
 const router = {
     "ping": handlers.ping,
-    "users": handlers.users
+    "users": handlers.users,
+    "tokens": handlers.tokens
 };
