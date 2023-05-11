38df6113966a19a032123763947816ed68904bdc
BrightOlawale <oolawalebright@gmail.com>
2023-05-11 14:48:56 +0100

    feat: list function added to dataLogic & worker file added

---
+++ test.js
@@ -0,0 +1,20 @@
+// Description: TESTING
+
+// Dependencies
+// const dataLogic = require('./lib/dataLogic');
+
+// dataLogic.create('test', 'testFile', {'name': 'Olawale Olatunji Bright'}, function(err){
+//     console.error('Error:', err);
+// })
+
+// dataLogic.read('test', 'testFile', function(err, data){
+//     console.log(`The Error is ${err} and the data is ${data}`);
+// })
+
+// dataLogic.update('test', 'testFile', {'name': 'Olawale Bright', 'Email': 'oolawalebright@gmail.com'}, function(err){
+//     console.error(`The Error is ${err}`);
+// })
+
+// dataLogic.delete('test', 'testFile', function(err){
+//     console.log(`The Error is ${err}`);
+// })
