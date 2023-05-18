a5adbe017c13f594e31a351b916fef07104b9049
BrightOlawale <oolawalebright@gmail.com>
2023-05-18 17:29:25 +0100

    [feat]: template Global variable

---
+++ templateData.js
@@ -0,0 +1,13 @@
+// Description File for holding template data like Globals
+
+const templateData = {
+    'templateGlobals': {
+        'orgName' : 'Golden, Inc',
+        'appName' : 'UptimeChecker',
+        'yearCreated' : '2023',
+        'baseURL' : 'http://localhost:3000/'
+    }
+}
+
+// Export variables
+module.exports = templateData;
